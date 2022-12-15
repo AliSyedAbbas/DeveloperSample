74240763abb283c931d76745df5ec10ffae78412
AliSyedAbbas <120688612+AliSyedAbbas@users.noreply.github.com>
2022-12-15 17:53:53 -0500

    Update Algorithms.cs
    This is Syed Ali Abbas. Paul Held <PaulH@sds.io> has sent me link of this test so I am submitting my request with code changes as per the coding task.
    
    Thanks,
    Syed
    aahj11787@gmail.com

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,38 @@
 using System;
+using System.Linq;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int num)
+        {
+            int n = 1;
+            while(num > 0)
+            {
+                n = num * n;
+                num--;
+            }
+            return n;
+        }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+        public static string FormatSeparators(params string[] items)
+        {            
+            string[] resultArr;
+
+            int len = items.Length - 1;
+
+            string lastVal = items[len];
+
+            var arrayList = items.ToList();
+
+            arrayList.RemoveAt(len)	;
+
+            resultArr = arrayList.ToArray();
+
+            string resultStr = String.Format("{0} and {1}", string.Join(",", resultArr), lastVal);
+
+            return resultStr;		
+        }
     }
 }
