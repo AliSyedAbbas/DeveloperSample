6aec6242c16c6865ca4dfad29dbb878b961d9496
AliSyedAbbas <120688612+AliSyedAbbas@users.noreply.github.com>
2022-12-15 19:00:05 -0500

    Update Container.cs
    This is Syed Ali Abbas. Paul Held <PaulH@sds.io> has sent me link of this test so I am submitting my request with code changes as per the coding task.
    
    Thanks,
    Syed
    aahj11787@gmail.com

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+	private Dictionary<Type, Type> dictImplementation = new Dictionary<Type, Type>();
+
+        public void Bind(Type type1, Type type2)
+        {
+            dictImplementation.Add(type1, type2);
+        }
+
+        public T Get<T>()
+        {
+            if (dictImplementation.TryGetValue(typeof(T), out Type type))
+            {
+                var obj = Activator.CreateInstance(type);
+                return (T)obj;
+            }
+            return default(T);
+        }
     }
 }
