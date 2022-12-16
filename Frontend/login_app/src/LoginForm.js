765b2f6a50ecc9a026b13ff6bb885b9a520b5c0b
AliSyedAbbas <120688612+AliSyedAbbas@users.noreply.github.com>
2022-12-15 19:22:18 -0500

    Update LoginForm.js
    This is Syed Ali Abbas. Paul Held <PaulH@sds.io> has sent me link of this test so I am submitting my request with code changes as per the coding task.
    
    Thanks,
    Syed
    aahj11787@gmail.com

--- Frontend/login_app/src/LoginForm.js
+++ Frontend/login_app/src/LoginForm.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React,  { useState } from "react";
 import './LoginForm.css';
 
 const LoginForm = (props) => {
-	const handleSubmit = (event) =>{
+    const [data, setData] = useState({})
+
+	const doUpdateData = e => {
+        setData({
+            ...data,
+            [e.target.id]: e.target.value
+        })
+    }
+	const doSubmit = (event) =>{
 		event.preventDefault();
 
 		props.onSubmit({
@@ -15,10 +23,10 @@ const LoginForm = (props) => {
 		<form className="form">
 			<h1>Login</h1>
 			<label htmlFor="name">Name</label>
-			<input type="text" id="name" />
+			<input type="text" id="name" onChange={doUpdateData}/>
 			<label htmlFor="password">Password</label>
-			<input type="password" id="password" />
-			<button type="submit" onClick={handleSubmit}>Continue</button>
+			<input type="password" id="password" onChange={doUpdateData}/>
+			<button type="submit" onClick={doSubmit}>Continue</button>
 		</form>
 	)
 }
